eefaed6c187fa636a616ecc50278b048f241655c
Mathieu Malisani <mathieu@Mac.home>
2025-07-28 19:47:23 +0200

    added vercel insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Noto_Sans, Noto_Serif } from 'next/font/google';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import '@/styles/globals.css';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const notoSans = Noto_Sans({
   subsets: ['latin'],
@@ -35,6 +36,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
+      <SpeedInsights/>
       <body suppressHydrationWarning className={`${notoSans.variable} ${notoSerif.variable} flex flex-col min-h-screen bg-soft`}>
         <Header />
         <main className="flex-1 container mx-auto px-4">{children}</main>
